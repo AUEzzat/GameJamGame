b5630766d65e167b48bfc6c31786686d55ab36fe
AUEzzat <amr.m.ezzat@gmail.com>
2018-01-27 18:55:13 +0200

    player mesh added

---
+++ Assets/Scripts/CatchingControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchingControll : MonoBehaviour
+{
+
+    private bool triggerEntered;
+    Collider obj;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetType() == typeof(SphereCollider))
+        {
+            triggerEntered = true;
+            obj = other;
+            Debug.Log("trigger entered");
+        }
+
+    }
+    void Start()
+    {
+        triggerEntered = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick2Button5) && triggerEntered == true)
+        {
+            Debug.Log("Key pressed");
+            obj.transform.position = this.transform.position;
+            obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            obj.transform.SetParent(this.transform, false);
+        }
+    }
+}
